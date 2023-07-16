742d9e2333e8d8ebd4365a9b951abecc7e6a354a
JDiaz <jdiaz_007@outlook.com>
2023-07-16 14:34:46 -0700

    initial commit

---
+++ logic.js
@@ -0,0 +1,20 @@
+const warrantyFormHeader = document.createElement ('h1')
+warrantyFormHeader.textContent = 'Warranty Information'
+document.body.append(warrantyFormHeader)
+
+const firstName = document.createElement ('label')
+firstName.textContent = 'First Name'
+document.body.append(firstName)
+// const firstName = document.createElement ('input')
+// firstName.setAttribute('type', 'text')
+// document.body.append(firstName)
+
+
+// Buttons start here
+
+const button = document.createElement ('button')
+button.textContent
+ = 'Save'
+document.body.append(button)
+
+
